26252cf6393b3dedf9da5253d3b798b6f42b0f01
HarshithaNagaraj2512 <96883154+HarshithaNagaraj2512@users.noreply.github.com>
2022-01-24 07:10:36 +0530

    Add files via upload

---
+++ Assignment_2.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. two values for boolean data type are true and false.
+# 2. and, or ,  not
+# 3. and True if both  are true
+#     or True if either of one is true
+#     not true only if none is true
+# 4. False
+#    False
+#    True
+#    Flase
+#    True
+# 5. < (Less than)
+#    > (Greater than)
+#    >= (Greater than or eual to)
+#    <=(Less than or equal to)
+#    !=(not equal to)
+#    ==(equal to)
+# 6. Equality(==) operator check whether the value is equal to a given value where as assignment operator(=) assigns a value to variable.
+# Equality operator
+# 
+# l=[1,2,3,4]
+# for i in l :
+#     if (i==2):
+#         print('got it')
+#     else :
+#         print('not in list')
+# 
+#  assign:
+#  l=[1,2,3,4]
+#  
+#  7. 
+#  spam = 10
+# if spam==10: [1st block]
+#     print('eggs')
+# elif spam>5: [2nd block]
+#         print('bacon')
+# else:[3rd block]
+#     print('ham')
+#     print('spam')
+#     print('spam')
+#     
+# 8. spam = int(input())
+# if spam==1:
+#     print('Hello')
+# elif spam==2:
+#         print('Howdy')
+# else:
+#     print('greetings!!')
+# 9. interrupt kernel(pause)
+# 10. Break terminates iteration of the loop and switch statement whereas continue used within a loop
+# 11. There is no difference all three does same i.e creates a range from 0 to 9
+# 12.##for loop
+# for i in range(1,11):
+#     print(i)
+#  ##while loop  
+#     a= True
+# while a:
+#     for i in range(1,11):
+#         print(i)
+#     break  
+#     
+# 13. import spam()
+# spam.bacon()
