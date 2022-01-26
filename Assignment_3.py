5357aba62aba00b6786ba48c9ace10b180368729
HarshithaNagaraj2512 <96883154+HarshithaNagaraj2512@users.noreply.github.com>
2022-01-26 23:34:03 +0530

    Add files via upload

---
+++ Assignment_3.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. Functions are advantageous because we can define them once in our program and can be called n number of times.
+# 2. When it is called
+# 3. def funt():
+# 4. function is to write to perform certain task, wheras function call is to invoke the function written.
+# 5. Global scope: which is having scope throught the program
+#     Local scope: local variable is declared inside a function.
+# 6. Local variable will be destroyed, since local variable has scope only inside a function we can use the same variable name in other function.
+# 7. return is the key word in pyhthon used to end the execution of the function and execute main program.
+# 8. If there is no return it returns to calling function and the function is called undefined.
+# 9. By using global keyword.
+# 10. None type
+# 11. import a module named areallyourpetsnamederic
+# 12. the function can be called using spam.bacon()
+# 13. Exception handling 
+#     try:
+#     except:
+# 14. try is used to run the test code return
+#     except runs if an error occurs.
