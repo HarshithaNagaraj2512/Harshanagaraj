bb591f333f4c79a5e3572390c107bf7e630330dd
HarshithaNagaraj2512 <96883154+HarshithaNagaraj2512@users.noreply.github.com>
2022-01-27 00:20:25 +0530

    Add files via upload

---
+++ Assignment_4.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. A list can be created using [] parantheces
+# 2. spam.append('hello')
+# 3. d
+# 4. d
+# 5. ['a', 'b']
+# 6. 1
+# 7. [3.14, 'cat', 11, 'cat', True, 99] adds the value 99 at the end of the list
+# 8. fisrt str 'cat' at index 1  has been removed
+# 9. list concatenation(+) comibine two or more list into a single list where as list replication(*) multiplies the string.
+# 10. append adds an element at the end of the list where as using insert we can add at particualar index.
+# 11. remove and pop
+# 12. list is collection of values which can be int,str, complex or an another list and string is sequence of characters.
+# 13. a. List consists of mutable objects whereas tuple consists of immutable objects
+#     b. An element of the list can be removed or replaced whereas it is not possible in case of tuple
+#     c. List has large memory since it is stored in two blocks of memory and tuples hav single block of memory which makes tuple consume less memory
+#     d. A list data is stored [] and tuples  data is stored in ().
+# 14. By adding a comma after the element 42
+# 15. t=(1,2,3)
+#     l = list(t)
+#     print(l)
+# 16. they can be tuples dictionary 
+# 17. copy.copy()constructs a new compound object and then  inserts references into it to the objects found in the original. 
+#     copy.deepcopy() constructs a new compound object and then, recursively, inserts copies into it of the objects found in the original.
