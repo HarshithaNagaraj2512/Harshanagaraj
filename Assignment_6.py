cde1d60857de51bc1f02089f3542f1cb20bfac31
HarshithaNagaraj2512 <96883154+HarshithaNagaraj2512@users.noreply.github.com>
2022-02-05 13:28:41 +0530

    Add files via upload

---
+++ Assignment_6.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1.
+# a) “\t” is a tab,
+# b) “\n” is a new line, 
+# c) “\r” is a carriage return
+# 2. n stands for new line
+# t stands for space oor tab
+# 3.  "\\" within the string literal
+# 4. since it is included in "" 
+# 5. print ('hello'),
+# print ('good day')  
+#  use comma at the end of the print statement
+#  6. 'Hello,world!'[1] gives index of 1 i.e e
+#  'Hello,world!'[0:5] prints the idex range from 0 to 5 i.e Hello
+#  'Hello,world!'[:5]prints the idex range from 0 to 5 i.e Hello
+#  'Hello,world!'[3:], prints from index 3 i.e 'lo,world!'
+#  7. 'Hello'.upper() prints in uppercase i.e HELLO
+#  'Hello'.upper().isupper() returns true if all chars are in upper case
+#  'Hello'.upper().lower() converts all characters into lower case
+#  8. 'Remember,remember,the fifth of july.'.split()
+#  output : ['Remember,remember,the', 'fifth', 'of', 'july.']
+#  '-'.join('the only one.'.split())
+#  output : 'the-only-one.'
+#  9. ljust(),rjust() , center()
+#  10.strip()
