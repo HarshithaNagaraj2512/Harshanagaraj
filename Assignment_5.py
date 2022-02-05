ca86155036316da1758b01f850c1ba0881389a1f
HarshithaNagaraj2512 <96883154+HarshithaNagaraj2512@users.noreply.github.com>
2022-02-05 13:04:51 +0530

    Add files via upload

---
+++ Assignment_5.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. emptydict = {}
+#    print (emptydict)
+# 2. dict ={'foo':42}
+#    {'foo': 42}
+# 3. list need no to have homogneous it can have data types like integers,string float whereas in dictionory it is a key value pair in which collection of data values and store like mapping
+# 4. There is no difference
+# 5. There is no difference cat always check wheteher there is a key in dictionory and spam checks for a value in dictionary
+# 6. There is no difference. The in operator checks whether a value exists as a key in the dictionary. 'cat' in spam checks whether there is a 'cat' key in the dictionary, while 'cat' in spam. values() checks whether there is a value 'cat' for one of the keys in spam.
+# 7. spam{'color':'black'}
+# 8.
+# test_dict = {'student' : {'name' : 'harsh', 'age' : '25'}, 'branch' : {'year' : 'final'}}
+#   
+# print("The original dictionary is : " +  str(test_dict))
+# 
+# print("The Pretty Print dictionary is : ")
+# for sub in test_dict:
+#     print (sub)
+#     for sub_nest in test_dict[sub]:
+#         print (sub_nest, ':', test_dict[sub][sub_nest])
+# 
+
+# In[ ]:
+
+
+
+
